20e0587bc2838192ef4862e8d04e874b1f0e8e52
nikita_kogolenok <nikiton17ns@gmail.com>
2022-03-23 14:47:16 +0300

    Add Cell with TableViewCell and CollectionViewCell

---
+++ CollectionViewWithTableCell/Cell/CollectionCell/MyCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionViewWithTableCell
+//
+//  Created by Никита Коголенок on 23.03.22.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+
+    // MARK: - View Cycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
---
+++ CollectionViewWithTableCell/Cell/TableCell/CollectionTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionTableViewCell.swift
+//  CollectionViewWithTableCell
+//
+//  Created by Никита Коголенок on 23.03.22.
+//
+
+import UIKit
+
+class CollectionTableViewCell: UITableViewCell {
+
+    // MARK: - Life Cycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
