f395a7e5024be143d6e0cc8e20248024dc0ec612
nikita_kogolenok <nikiton17ns@gmail.com>
2022-03-23 12:51:02 +0300

    Initial Commit

---
+++ CollectionViewWithTableCell/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CollectionViewWithTableCell
+//
+//  Created by Никита Коголенок on 23.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - View Live Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
